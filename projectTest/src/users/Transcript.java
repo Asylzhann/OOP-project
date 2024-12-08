cdb4c73b2e34344440649e3608f84db7089b25cf
Asylzhan <75136099+Asylzhann@users.noreply.github.com>
2024-12-08 18:26:40 +0500

    Changed Transcript class

--- projectTest/src/users/Transcript.java
+++ projectTest/src/users/Transcript.java
@@ -3,17 +3,17 @@ package users;
 import java.util.List;
 
 public class Transcript {
-    private List<Semester> semesters;
-    private double totalGpa;
-    private int totalCredits;
-
-    public Transcript(List<Semester> semesters, double totalGpa, int totalCredits) {
-        this.semesters = semesters;
-        this.totalGpa = totalGpa;
-        this.totalCredits = totalCredits;
-    }
-
-    public String getDetails() {
-        return "Total GPA: " + totalGpa + ", Total Credits: " + totalCredits;
-    }
+//    private List<Semester> semesters;
+//    private double totalGpa;
+//    private int totalCredits;
+//
+//    public Transcript(List<Semester> semesters, double totalGpa, int totalCredits) {
+//        this.semesters = semesters;
+//        this.totalGpa = totalGpa;
+//        this.totalCredits = totalCredits;
+//    }
+//
+//    public String getDetails() {
+//        return "Total GPA: " + totalGpa + ", Total Credits: " + totalCredits;
+//    }
 }
