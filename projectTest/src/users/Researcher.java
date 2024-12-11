31d6ed20037101429214ac50ce85d07cfa8e3519
Asylzhan <75136099+Asylzhann@users.noreply.github.com>
2024-12-11 10:43:04 +0500

    Delete projectTest/src/users/Researcher.java

--- projectTest/src/users/Researcher.java
+++
@@ -1,9 +0,0 @@
-package users;
-
-import java.util.Comparator;
-
-public interface Researcher {
-    void addPaper(ResearchPaper paper);
-    void printPapers(Comparator<ResearchPaper> comparator);
-    double calculateHIndex();
-}
