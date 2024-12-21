0ddd6c8b11d059eb98fd9d632f22ad7f947ad53f
Asylzhan <75136099+Asylzhann@users.noreply.github.com>
2024-12-21 18:15:27 +0500

    Delete projectTest/projectTest directory

--- projectTest/projectTest/src/enums/StudentDegree.java
+++
@@ -1,5 +0,0 @@
-package enums;
-
-public enum StudentDegree {
-	BACHELOR, MASTER, PHD;
-}
--- projectTest/projectTest/src/enums/TeacherTitle.java
+++
@@ -1,8 +0,0 @@
-package enums;
-
-public enum TeacherTitle {
-    TUTOR,
-    LECTOR,
-    SENIOR_LECTOR,
-    PROFESSOR
-}
